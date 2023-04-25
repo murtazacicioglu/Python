2da349038e1632f447ff81fd8c4777e5aeb6ef71
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-04-26 01:31:31 +0300

    use from expressions

--- 22-23 Proje/1. Grup/preprocessor.py
+++ 22-23 Proje/1. Grup/preprocessor.py
@@ -5,17 +5,18 @@
 #
 #       preprocessor.py
 import re as regex
+import expressions
 
 
 class Preprocessor:
     def __init__(self, trim_spaces=True, clear_comments=True, clear_newline=True):
         self.filters = {}
         if trim_spaces:
-            self.filters["trim_spaces"] = lambda b: regex.sub('[\t ]+', ' ', b).strip()
+            self.filters["trim_spaces"] = lambda b: regex.sub(expressions.exp_preproc_space, ' ', b).strip()
         if clear_comments:
-            self.filters["clear_comments"] = lambda b: regex.sub('\s*;.+', '', b)
+            self.filters["clear_comments"] = lambda b: regex.sub(expressions.exp_preproc_comment, '', b)
         if clear_newline:
-            self.filters["clear_newline"] = lambda b: regex.sub('^(?:[\t ]*(?:\r?\n|\r))+', '', b)
+            self.filters["clear_newline"] = lambda b: regex.sub(expressions.exp_preproc_newline, '', b)
 
     def format(self, buffer: str) -> str:
         """
