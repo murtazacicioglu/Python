eec91f4e97e8359bb01c1b59b4d7776ef16febf5
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-04-25 21:31:18 +0300

    + base for instructions

--- 22-23 Proje/1. Grup/instruction.py
+++ 22-23 Proje/1. Grup/instruction.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 #
-#         Ferit Yiğit BALABAN <fybalaban@fybx.dev>
+#       Ferit Yiğit BALABAN <fybalaban@fybx.dev>
 #
+#       instruction.py
+import data_types
 
 
-def instr_add(rd: str, rs1: str, rs2: str):
-    
+class Instruction:
+    type: str
+    width = 32
+    instruction = data_types.Word(0)
+
+    def __init__(self, inst_type: str):
+        inst_type = inst_type.lower()
+        if inst_type != "r" and inst_type != "i" and inst_type != "sb" and inst_type != "uj":
+            raise AttributeError("Instruction tipi R, I, SB veya UJ olmalı!")
+        self.type = inst_type
+
+    def set_opcode(self, int_value: int):
+        self.instruction.set_rrange(6, 0, bin(int_value)[2:])
