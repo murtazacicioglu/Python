93e274c5735f4ff81fafa68cd3022b23611692c4
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-04-26 00:56:13 +0300

    + ISA_RV32I

---
+++ 22-23 Proje/1. Grup/isa_rv32i.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#
+#       Ferit Yiğit BALABAN <fybalaban@fybx.dev>
+#
+#       isa_rv32i.py
+import instruction
+
+
+class ISA_RV32I:
+    inst: dict[instruction.Instruction]
+
+    def __init__(self):
+        pass
+
