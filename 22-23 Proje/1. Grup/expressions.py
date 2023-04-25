fa579e59d0bf781b827942d90b5c307844e698e3
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-04-25 19:17:44 +0300

    + exp: check rv32i register address

---
+++ 22-23 Proje/1. Grup/expressions.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#
+#       Ferit Yiğit BALABAN <fybalaban@fybx.dev>
+#
+#       expressions.py
+#       proje boyunca çeşitli yerlerde kullanılan tüm reg. expresionlar
+
+
+exp_regaddr_rv32i = "^x(0|[1-2][0-9]|3[0-1])$"
