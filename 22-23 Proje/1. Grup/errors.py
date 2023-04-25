4de3f84e0d1f8f5c83f0bb45a11f375e786637c0
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-04-26 00:41:42 +0300

    + i didn't know how python work, my assumption was wrong :)

--- 22-23 Proje/1. Grup/errors.py
+++ 22-23 Proje/1. Grup/errors.py
@@ -3,10 +3,8 @@
 #       Ferit Yiğit BALABAN <fybalaban@fybx.dev>
 #
 #       errors.py
-import instruction
 
 
 class InstructionTypeError(Exception):
-    def __init__(self, inst: instruction.Instruction):
-        message = f"{inst.type.name} tipli komutta uyumsuz bir çağrı yürütüldü."
+    def __init__(self, message: str):
         super().__init__(message)
