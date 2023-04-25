9d9c16690185cd3494c1b79a1d206b65e3ae5be6
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-04-25 23:26:49 +0300

    + isa formatına göre verileri yükleyen metotlar

--- 22-23 Proje/1. Grup/instruction.py
+++ 22-23 Proje/1. Grup/instruction.py
@@ -17,5 +17,44 @@ class Instruction:
             raise AttributeError("Instruction tipi R, I, SB veya UJ olmalı!")
         self.type = inst_type
 
-    def set_opcode(self, int_value: int):
+    def set_opcode(self, int_value: int) -> None:
+        """
+        Verilen ondalık sayıyı opcode (6-0) olarak ayarlar.
+        :param int_value: Ayarlanacak sayı
+        """
         self.instruction.set_rrange(6, 0, bin(int_value)[2:])
+
+    def set_rd(self, int_value: int) -> None:
+        """
+        Verilen ondalık sayıyı rd (11-7) olarak ayarlar.
+        :param int_value: Ayarlanacak sayı
+        """
+        self.instruction.set_rrange(11, 7, bin(int_value)[2:])
+
+    def set_rs1(self, int_value: int) -> None:
+        """
+        Verilen ondalık sayıyı rs1 (19-15) olarak ayarlar.
+        :param int_value: Ayarlanacak sayı
+        """
+        self.instruction.set_rrange(19, 15, bin(int_value)[2:])
+
+    def set_rs2(self, int_value: int) -> None:
+        """
+        Verilen ondalık sayıyı rs2 (24-20) olarak ayarlar.
+        :param int_value: Ayarlanacak sayı
+        """
+        self.instruction.set_rrange(24, 20, bin(int_value)[2:])
+
+    def set_funct3(self, int_value: int) -> None:
+        """
+        Verilen ondalık sayıyı funct3 (14-12) olarak ayarlar.
+        :param int_value: Ayarlanacak sayı
+        """
+        self.instruction.set_rrange(14, 12, bin(int_value)[2:])
+
+    def set_funct7(self, int_value: int) -> None:
+        """
+        Verilen ondalık sayıyı funct7 (31-25) olarak ayarlar.
+        :param int_value: Ayarlanacak sayı
+        """
+        self.instruction.set_rrange(31, 25, bin(int_value)[2:])
