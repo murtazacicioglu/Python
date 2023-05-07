04cc95fb5af26f7bfc95a6d7f6fd0d56578ffa9c
edayildizz <98519482+edayildizz@users.noreply.github.com>
2023-05-07 23:39:40 +0300

    arayüz
    arayüz eklendi
    
    Co-Authored-By: Gülsüm <100771793+gulsumkuntur@users.noreply.github.com>

---
+++ 22-23 Proje/3. Grup/arayüz/legend_test.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QTimer,QTime,QDate
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+from legendUi import Ui_MainWindow
+
+class MainThread(QThread):
+    def __init__(self):
+        super(MainThread, self).__init__()
+
+execution = MainThread()
+
+class Mywindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.startgif)
+        
+    def startgif(self):
+        self.ui.movie = QtGui.QMovie("22-23 Proje/3. Grup/arayüz/circle.gif")
+        self.ui.circle.setMovie(self.ui.movie)
+        self.ui.movie.start()
+        execution.start()
+
+
+app = QApplication(sys.argv)
+athena = Mywindow()
+athena.show()
+exit(app.exec_())
