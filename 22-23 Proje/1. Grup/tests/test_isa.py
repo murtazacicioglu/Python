25755fdea74f4d0ca5eeb2bb7ae9e3e6316eb6a1
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-10 18:20:15 +0300

    test: isa_inv()

--- 22-23 Proje/1. Grup/tests/test_isa.py
+++ 22-23 Proje/1. Grup/tests/test_isa.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import processor
 from data_types import Word
-from isa import isa_add
+from isa import isa_add, isa_inv
 
 
 class TestIsaAdd(TestCase):
@@ -33,3 +33,22 @@ class TestIsaAdd(TestCase):
         proc.registers['x2'] = Word(0)
         isa_add(proc, 'x3', 'x1', 'x2')
         assert proc.registers['x3'].value == 10
+
+
+class TestIsaInv(TestCase):
+    def test_isa_inv_positive(self):
+        proc = processor.Processor()
+        proc.registers['x1'] = Word(5)
+        isa_inv(proc, 'x1')
+        assert proc.registers['x1'].value == -5
+
+    def test_isa_inv_negative(self):
+        proc = processor.Processor()
+        proc.registers['x2'] = Word(-10)
+        isa_inv(proc, 'x2')
+        assert proc.registers['x2'].value == 10
+
+    def test_isa_inv_zero(self):
+        proc = processor.Processor()
+        isa_inv(proc, 'x0')
+        assert proc.registers['x0'].value == 0
