7a9a8df4f1df247323db5d9bdb2200978888ba88
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-10 18:46:20 +0300

    isa: isa_sub

--- 22-23 Proje/1. Grup/isa.py
+++ 22-23 Proje/1. Grup/isa.py
@@ -14,3 +14,16 @@ def isa_add(proc: processor.Processor, rd: str, rs1: str, rs2: str):
 def isa_inv(proc: processor.Processor, rd: str):
     proc.registers[rd] = Word(-1 * proc.registers[rd].value)
 
+
+def isa_sub(proc: processor.Processor, rd: str, rs1: str, rs2: str):
+    _rs1 = proc.registers[rs1].value
+    _rs2 = proc.registers[rs2].value
+    _rd = Word(0)
+
+    _rd = Word(_rs1 - _rs2)
+    proc.flags["C"] = Word(0 if _rs2 >= _rs1 else 1)
+    proc.flags["Z"] = Word(1 if _rd.value == 0 else 0)
+    proc.flags["S"] = Word(0 if _rd.value >= 0 else 1)
+    proc.flags["V"] = Word(1 if (_rs1 >= 0 > _rs2 and _rd.value < 0) or (_rs1 < 0 <= _rs2 and _rd.value >= 0) else 1)
+    proc.registers[rd] = _rd
+
