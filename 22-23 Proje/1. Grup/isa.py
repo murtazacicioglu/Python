779ff17035cc4db20f5dc72c7105727b037cfbc4
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-10 18:20:37 +0300

    isa: + isa_inv()

--- 22-23 Proje/1. Grup/isa.py
+++ 22-23 Proje/1. Grup/isa.py
@@ -10,3 +10,7 @@ from data_types import Word
 def isa_add(proc: processor.Processor, rd: str, rs1: str, rs2: str):
     proc.registers[rd] = Word(proc.registers[rs1].value + proc.registers[rs2].value)
 
+
+def isa_inv(proc: processor.Processor, rd: str):
+    proc.registers[rd] = Word(-1 * proc.registers[rd].value)
+
