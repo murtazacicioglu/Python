e53479bc43b6834a1bdd6c77dd8877b98d1d5893
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-10 18:13:14 +0300

    test: isa_add

---
+++ 22-23 Proje/1. Grup/tests/test_isa.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+import processor
+from data_types import Word
+from isa import isa_add
+
+
+class Test_ISA_add(TestCase):
+    def test_isa_add_positive_numbers(self):
+        proc = processor.Processor()
+        proc.registers['x1'] = Word(10)
+        proc.registers['x2'] = Word(20)
+        isa_add(proc, 'x3', 'x1', 'x2')
+        assert proc.registers['x3'].value == 30
+
+    def test_isa_add_negative_numbers(self):
+        proc = processor.Processor()
+        proc.registers['x1'] = Word(-10)
+        proc.registers['x2'] = Word(-20)
+        isa_add(proc, 'x3', 'x1', 'x2')
+        assert proc.registers['x3'].value == -30
+
+    def test_isa_add_mixed_numbers(self):
+        proc = processor.Processor()
+        proc.registers['x1'] = Word(10)
+        proc.registers['x2'] = Word(-20)
+        isa_add(proc, 'x3', 'x1', 'x2')
+        assert proc.registers['x3'].value == -10
+
+    def test_isa_add_zero(self):
+        proc = processor.Processor()
+        proc.registers['x1'] = Word(10)
+        proc.registers['x2'] = Word(0)
+        isa_add(proc, 'x3', 'x1', 'x2')
+        assert proc.registers['x3'].value == 10
