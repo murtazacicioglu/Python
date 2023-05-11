1fbc5f82ae779ecbe274e9ecb45f0c39b60328ad
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-11 13:36:26 +0300

    isa: ^ isa_add()

--- 22-23 Proje/1. Grup/isa.py
+++ 22-23 Proje/1. Grup/isa.py
@@ -8,7 +8,17 @@ from data_types import Word
 
 
 def isa_add(proc: processor.Processor, rd: str, rs1: str, rs2: str):
-    proc.registers[rd] = Word(proc.registers[rs1].value + proc.registers[rs2].value)
+    _rs1 = proc.registers[rs1].value
+    _rs2 = proc.registers[rs2].value
+    _rd = Word(0)
+
+    _rd = Word(proc.registers[rs1].value + proc.registers[rs2].value)
+
+    # carry bayrağı hesaplama, işaretsiz sayı desteği yok
+    proc.flags["Z"] = Word(1 if _rd.value == 0 else 0)
+    proc.flags["S"] = Word(0 if _rd.value >= 0 else 1)
+    proc.flags["V"] = Word(1 if (_rs1 > 0 > _rd.value and _rs2 > 0) or (_rs1 < 0 < _rd.value and _rs2 < 0) else 0)
+    proc.registers[rd] = _rd
 
 
 def isa_inv(proc: processor.Processor, rd: str):
