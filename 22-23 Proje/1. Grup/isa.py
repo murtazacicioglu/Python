2d0bfc1f9b1adac9e2759ffb89e81e7a8c6d858d
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-11 13:42:45 +0300

    isa: update boundaries

--- 22-23 Proje/1. Grup/isa.py
+++ 22-23 Proje/1. Grup/isa.py
@@ -38,9 +38,10 @@ def isa_sub(proc: processor.Processor, rd: str, rs1: str, rs2: str):
     _rd = Word(0)
 
     _rd = Word(_rs1 - _rs2)
+
     proc.flags["C"] = Word(0 if _rs2 >= _rs1 else 1)
     proc.flags["Z"] = Word(1 if _rd.value == 0 else 0)
     proc.flags["S"] = Word(0 if _rd.value >= 0 else 1)
-    proc.flags["V"] = Word(1 if (_rs1 >= 0 > _rs2 and _rd.value < 0) or (_rs1 < 0 <= _rs2 and _rd.value >= 0) else 1)
+    proc.flags["V"] = Word(1 if (_rs1 > 0 > _rs2 and _rd.value < 0) or (_rs1 < 0 < _rs2 and _rd.value > 0) else 0)
     proc.registers[rd] = _rd
 
