6a9403f745c4141980aee40210cb5244de5af764
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-13 02:11:34 +0300

    i'm having a stroke

--- 22-23 Proje/1. Grup/parser.py
+++ 22-23 Proje/1. Grup/parser.py
@@ -3,13 +3,15 @@
 #       Ferit Yiğit BALABAN, <fybalaban@fybx.dev>
 #
 #       parser.py
+import copy
+
 import data_types
 import memory
 import preprocessor
 
 
 BRANCHING = ["jmp", "beq", "bne", "bge", "ble"]
-MEMACCESS = ["lfm", "stm"]
+MEMACCESS = ["lfm", "stm", "mvi"]
 
 
 class Parser:
@@ -24,7 +26,10 @@ class Parser:
         :return: Çevrilmiş satırları içeren dizi
         """
         sections = {}
+        stores = {}
         intermediate_code = []
+        current_section = ""
+        section_offset = 0
 
         for line in source_code.split('\n'):
             line = line.strip()
@@ -33,6 +38,10 @@ class Parser:
             if line.startswith('.'):
                 section_name = line.strip('.')
                 sections[section_name] = len(intermediate_code)
+                current_section = section_name
+            elif current_section == "store" and line[0:3] == "dbs":
+                parts = line.split(maxsplit=2)
+                stores[parts[1].removesuffix(",")] = [len(parts[2]) - 1, parts[2], 0]
             else:
                 instruction = line.split(maxsplit=1)
                 if len(instruction) == 2:
@@ -41,22 +50,45 @@ class Parser:
                 elif len(instruction) == 1:
                     intermediate_code.append(instruction[0])
 
+        if stores.keys():
+            temp = copy.deepcopy(intermediate_code)
+            intermediate_code = []
+            mem_offset = 0
+            for key in stores.keys():
+                stores[key][2] = mem_offset
+                string = stores[key][1][1:-1]
+                for char in string:
+                    intermediate_code.append(f"mvi x1 {data_types.Word(0).from_utf8(char).as_hexadecimal()}h")
+                    intermediate_code.append(f"stm x1 {data_types.Word(mem_offset).as_hexadecimal()}h")
+                    mem_offset += 1
+                intermediate_code.append(f"mvi x1 x0")
+                intermediate_code.append(f"stm x1 {data_types.Word(mem_offset).as_hexadecimal()}h")
+                mem_offset += 1
+
+            section_offset = len(intermediate_code)
+            intermediate_code = intermediate_code + temp
+
         for i, line in enumerate(intermediate_code):
             if line[0:3] in BRANCHING or line[0:3] in MEMACCESS:
                 parts = line.split()
                 section = parts[-1]
                 if section in sections.keys():
-                    parts[-1] = data_types.Word(sections[section]).as_hexadecimal() + "h"
+                    parts[-1] = data_types.Word(sections[section] + section_offset).as_hexadecimal() + "h"
                 elif section.startswith("[") and section.endswith("]"):
                     inner = section[1:-1]
-                    if "+" in inner:
+                    if inner in stores.keys():
+                        parts[-1] = data_types.Word(stores[inner][2]).as_hexadecimal() + "h"
+                    elif "+" in inner:
                         ip = inner.split("+")
                         parts[-1] = data_types.Word(
-                            sections[ip[0]] + data_types.Word(0).from_hex(ip[1][:-1]).value
+                            sections[ip[0]] + data_types.Word(0).from_hex(ip[1][:-1]).value + section_offset
                         ).as_hexadecimal() + "h"
                     else:
-                        parts[-1] = data_types.Word(0).from_hex(inner[:-1]).as_hexadecimal() + "h"
+                        parts[-1] = data_types.Word(
+                            data_types.Word(0).from_hex(inner[:-1]).value + section_offset
+                        ).as_hexadecimal() + "h"
                 intermediate_code[i] = " ".join(parts)
+
         return intermediate_code
 
     def parse_code(self, source_code: str) -> None:
@@ -64,7 +96,7 @@ class Parser:
         Kaynak kodu parse ederek orta formata çevirir ve __intermediate özelliğinde saklar.
         :param source_code: Assembly kaynak kodu
         """
-        pp = preprocessor.Preprocessor()
+        pp = preprocessor.Preprocessor(trim_spaces=False, clear_newline=False)
         clean_code = pp.format(source_code)
         self.__intermediate = self.__parse_code(clean_code)
 
