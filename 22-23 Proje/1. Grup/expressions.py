71292e3439d3765384c670c8b66b18542d20f09f
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-13 02:10:19 +0300

    erase lines with only semicolon

--- 22-23 Proje/1. Grup/expressions.py
+++ 22-23 Proje/1. Grup/expressions.py
@@ -8,5 +8,5 @@
 
 exp_regaddr_rv32i = "^x(0|[1-2][0-9]|3[0-1])$"
 exp_preproc_space = "[\t ]+"
-exp_preproc_comment = "\s*;.+"
+exp_preproc_comment = "\s*;.*"
 exp_preproc_newline = "^(?:[\t ]*(?:\r?\n|\r))+"
